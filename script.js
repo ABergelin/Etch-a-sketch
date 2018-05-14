fb7546ed8f6e4eefde4138e2f1423d7c076b29bf
Alex Bergelin <the_bergelin@hotmail.co.uk>
2018-05-14 21:23:08 +0100

    Added code to change the colour of the boxes.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+//Generate boxes
+makeGrid();
+
+//change colour
+const allCells = document.querySelectorAll('.floating-box')
+allCells.forEach(cell=>cell.addEventListener('mouseover', (e)=>{
+    cell.style.backgroundColor = 'Black';
+}));
+
+//Refresh button
+const refreshButton = document.getElementById('#refresh');
+refreshButton.addEventListener('onclick', function(){ alert("Hello World!"); });
+
 //Code to create the boxes on an Etch a Sketch
-const grid = document.querySelector('#gridContainer');
-for (var i = 1; i < 400; i++){
-	const box = document.createElement('div');
-	box.classList.add('floating-box');
-	grid.appendChild(box);
-}
+function makeGrid(){
+    const grid = document.querySelector('#gridContainer');
+    grid.innerHTML='';
+        for (var i = 1; i < 400; i++){
+            const box = document.createElement('div');
+            box.classList.add('floating-box');
+            grid.appendChild(box);
+        };
+};
+
+
