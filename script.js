8db9f0f1b630947cacaeb1a241bb5d7d9c60faa5
Alex Bergelin <the_bergelin@hotmail.co.uk>
2018-05-21 21:32:02 +0100

    Amend refresh button

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 //Generate boxes
 makeGrid();
 
-//change colour
-const allCells = document.querySelectorAll('.floating-box')
-allCells.forEach(cell=>cell.addEventListener('mouseover', (e)=>{
-    cell.style.backgroundColor = 'Black';
-}));
+
 
 //Refresh button
-const refreshButton = document.getElementById('#refresh');
-refreshButton.addEventListener('onclick', function(){ alert("Hello World!"); });
+const refreshButton = document.getElementById('refresh');
+refreshButton.addEventListener('click', makeGrid);
 
 //Code to create the boxes on an Etch a Sketch
 function makeGrid(){
     const grid = document.querySelector('#gridContainer');
     grid.innerHTML='';
-        for (var i = 1; i < 400; i++){
-            const box = document.createElement('div');
-            box.classList.add('floating-box');
-            grid.appendChild(box);
-        };
+    for (var i = 1; i < 400; i++){
+        const box = document.createElement('div');
+        box.classList.add('floating-box');
+        grid.appendChild(box);
+    };
+
+    //change colour
+    const allCells = document.querySelectorAll('.floating-box');
+    allCells.forEach(cell=>cell.addEventListener('mouseover', (e)=>{
+        cell.style.backgroundColor = 'Black';
+    }));
 };
-
-
